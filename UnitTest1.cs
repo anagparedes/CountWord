021ea16f62016bedbfb9f6c7387e36acadbf5489
anagparedes <anagparedesg@gmail.com>
2021-12-20 20:27:22 -0400

    Add unit test

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CountWord
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
